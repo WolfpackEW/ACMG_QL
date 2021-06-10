d4c5f8988226d0f1d969e15c640f9205985767de
Ethan Wen <ethanwen.sf@gmail.com>
2021-06-10 11:26:12 -0700

    third version of test project

--- src/templates/googleSheet.vue
+++ src/templates/googleSheet.vue
@@ -1,18 +1,20 @@
 <template>
   <Layout>
-    <h3>Testing page</h3>
-    <div>{{$page.googleSheet.Scientific_Name}}</div>
-    <div>{{$page.googleSheet.Common_Name}}</div>
-    <div>{{$page.googleSheet.Description}}</div>
-    <div>{{$page.googleSheet.Size__height__}}</div>
-    <div>{{$page.googleSheet.Size__width_}}</div>
-    <div>{{$page.googleSheet.Bloom_Season}}</div>
-    <div>{{$page.googleSheet.Pruning_Needs}}</div>
-    <div>{{$page.googleSheet.Water_Needs}}</div>
-    <div>{{$page.googleSheet.Exposure}}</div>
-    <div>{{$page.googleSheet.Type}}</div>
-    <div>{{$page.googleSheet.Location}}</div>
-    <div>{{$page.googleSheet.ID}}</div>
+    <v-container> 
+      <g-image alt ="Testing image" src="~/californian_poppy.jpg" width="300"/>
+      <h3>{{$page.googleSheet.Common_Name}}</h3>
+        <div>Scientific Name - {{$page.googleSheet.Scientific_Name}}</div>
+        <div>Description - {{$page.googleSheet.Description}}</div>
+        <div>Size(height) - {{$page.googleSheet.Size__height__}}</div>
+        <div>Size(width) - {{$page.googleSheet.Size__width_}}</div>
+        <div>Bloom Season - {{$page.googleSheet.Bloom_Season}}</div>
+        <div>Pruning Needs - {{$page.googleSheet.Pruning_Needs}}</div>
+        <div>Water Needs - {{$page.googleSheet.Water_Needs}}</div>
+        <div>Exposure - {{$page.googleSheet.Exposure}}</div>
+        <div>Type - {{$page.googleSheet.Type}}</div>
+        <div>Location - {{$page.googleSheet.Location}}</div>
+        <div>ID Number - {{$page.googleSheet.ID}}</div>
+    </v-container> 
   </Layout>
 </template>
 
